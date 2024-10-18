630f794ee78e06a62f07dff0224151bf6db62aa0
mrmuhib300@gmail.com <mrmuhib300@gmail.com>
2024-10-18 08:14:05 +0600

    add custom input

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ const output = document.getElementById("display__amount");
 const numberOfPeople = document.getElementById("number__ofpeople");
 const resetbtn = document.getElementById("resetbtn");
 let inputField = document.getElementById("input__fild");
+let customNumber = document.getElementById("customNumber");
+
+customNumber.addEventListener("input", () => {
+  customValue = Number(customNumber.value);
+  let inputValue = Number(inputField.value);
+
+  if (!inputValue) {
+    alert("Please enter a valid number.");
+    return false;
+  } else {
+    let people = 100 / customValue;
+    numberOfPeople.innerText = people;
+    let result = (inputValue * customValue) / 100;
+    output.innerText = result;
+  }
+});
 
 resetbtn.addEventListener("click", () => {
   output.innerText = "$0";
@@ -14,10 +30,16 @@ buttons.forEach((button) => {
     let inputValue = Number(inputField.value); // Parse the input value as a number
     let percentage = Number(button.value);
 
-    // Result this value as a percentage
-    let people = 100 / percentage;
-    let result = (inputValue * percentage) / 100;
-    output.innerText = result;
-    numberOfPeople.innerText = people;
+    if (!inputValue || !percentage) {
+      alert("Please enter a valid number.");
+      return false;
+    } else {
+      // Result this value as a percentage
+      let people = 100 / percentage;
+      let result = (inputValue * percentage) / 100;
+      output.innerText = result;
+      numberOfPeople.innerText = people;
+      inputField.value = "";
+    }
   });
 });
